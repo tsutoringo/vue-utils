94220e1f02180e6ce90427748e9da9fa9bedd50c
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2022-04-13 16:24:34 +0900

    Add force option

--- src/providers/localStorage.ts
+++ src/providers/localStorage.ts
@@ -3,9 +3,13 @@ import { InjectionKey, Ref, ref, watch } from "vue";
 export const useLocalStorage = () => {
   const store: {[key: string]: Ref<any>} = {} 
 
-  const get = <T = string>(key: string): Ref<T> => {
-    if (store[key]) return store[key];
+  const get = <T = string>(key: string, force: boolean = false): Ref<T> => {
     const value = localStorage.getItem(key);
+    if (store[key]) {
+      if (force) store[key].value = value;
+      return store[key];
+    }
+
     store[key] = ref(value);
 
     watch(store[key], (value) => {
