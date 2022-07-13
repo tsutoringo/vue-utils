bc38f2a9c6362d4a062cebc47b7f244fb8c9ab65
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2022-07-13 16:39:17 +0900

    move to vite

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import dts from 'vite-plugin-dts';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      name: 'vue-utils',
+      fileName: (format) => `vue-utils.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // Add external deps here
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+  plugins: [vue(), dts()]
+});
