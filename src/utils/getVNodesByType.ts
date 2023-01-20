a6711584d010803c4ddee05e1b4b2401afcb8e0c
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2023-01-21 00:12:21 +0900

    Add deep option

--- src/utils/getVNodesByType.ts
+++ src/utils/getVNodesByType.ts
@@ -1,11 +1,12 @@
 import { isVNode, VNode, VNodeArrayChildren, VNodeTypes } from 'vue';
 
-export const getVNodesByType = (target: VNodeArrayChildren, type: VNodeTypes): VNode[] => {
+export const getVNodesByType = (target: VNodeArrayChildren, type: VNodeTypes, deep: boolean = false): VNode[] => {
   const nodes: VNode[] = [];
   for (const node of target) {
     if (isVNode(node)) {
       if (node.type === type) {
         nodes.push(node);
+        if (deep && node.children && Array.isArray(node.children)) nodes.push(...getVNodesByType(node.children, type, true));
       } else if (node.children && Array.isArray(node.children)) {
         nodes.push(...getVNodesByType(node.children, type));
       }
